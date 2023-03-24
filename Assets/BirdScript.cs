4f18e15049ee916a0caa233b9de649b04b3fa20a
UItimate <52617311+UItimate@users.noreply.github.com>
2023-03-23 23:59:48 -0600

    feat: add game over screen, play again button, optimize code

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -6,6 +6,8 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +18,15 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
